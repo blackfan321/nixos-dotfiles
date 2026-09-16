a7d982313ac6d7b87e5afb186f50ee42537f4b39
Ivan Batrakov <blackfan321@gmail.com>
2026-09-16 03:17:15 +0300

    easyeffects: declarative preset

--- home-manager/modules/desktop/easyeffects.nix
+++ home-manager/modules/desktop/easyeffects.nix
@@ -1,9 +1,19 @@
 { pkgs, ... }:
 
+let
+  presets = pkgs.fetchFromGitHub {
+    owner = "JackHack96";
+    repo = "EasyEffects-Presets";
+    rev = "dd966e41ad9e44d4b11e19047f526ba718bbbe57";
+    hash = "sha256-JpQVWuEokBRu01xkGA22dPeV5Jo8Xzvfrg5oQ8RtIrI=";
+  };
+in
 {
   services.easyeffects = {
     enable = true;
-    package = pkgs.easyeffects;
-    # TODO: add declarative presets
+    preset = "Perfect EQ";
   };
+
+  xdg.dataFile."easyeffects/output/Perfect EQ.json".source =
+    "${presets}/Perfect EQ.json";
 }
